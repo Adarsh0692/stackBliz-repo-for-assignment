a90d8644128f499dba8379ce52e0a833b75c83ad
Adarsh0692 <126659184+Adarsh0692@users.noreply.github.com>
2023-05-11 20:26:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackBliz-repo-for-assignment
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ux2kws)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import Button from './component/Button'
+
+export default function App() {
+  return (
+    <div>
+      <Button/>
+    </div>
+  );
+}
