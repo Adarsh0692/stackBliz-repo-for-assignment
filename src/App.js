56aa39d77f6edecaee2f0bd416cf4a3ca59c2fc6
Adarsh0692 <126659184+Adarsh0692@users.noreply.github.com>
2023-05-11 21:02:49 +0530

    counter timer created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-import "./style.css";
-import Button from './component/Button'
+import React,{useEffect,useState} from "react";
+
 
 export default function App() {
+  const [time, setTime] = useState(60)
+
+useEffect(() => {
+  const timer = time >0 && setInterval(()=>{
+    setTime((pre) => pre - 1 )
+   
+  },1000)
+  return () => clearInterval(timer)
+} )
   return (
     <div>
-      <Button name="Button"/>
+      <h1>Counter Timer</h1>
+    {time} sec
     </div>
   );
 }
